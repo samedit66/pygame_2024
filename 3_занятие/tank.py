2a57bfd9a8ca7adb984ef5efa8faba1188b442eb
VlakBerr <boro46141@gmail.com>
2024-02-18 13:20:46 +0300

    сделал движение танка и разворот танка вверх и вправо

--- 3_занятие/tank.py
+++ 3_занятие/tank.py
@@ -3,17 +3,42 @@ import pygame
 class Tank(pygame.sprite.Sprite):
     def __init__(self, position: tuple[int, int], file_image: str):
         self.image = pygame.image.load(file_image).convert_alpha()
+        self.up_image = self.image
+        self.right_image = pygame.transform.rotate(self.image, -90)
+
         self.rect = self.image.get_rect(topleft=position)
+        self.move_x = 0
+        self.move_y = 0
+
+
+    def process_input(self):
+        self.move_x = self.move_y = 0
+
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_LEFT] or key[pygame.K_a]:
+            self.move_x = -2
+        elif key[pygame.K_RIGHT] or key[pygame.K_d]:
+            self.move_x = 2   
+            self.image = self.right_image
+        elif key[pygame.K_UP] or key[pygame.K_w]:
+            self.move_y = -2
+            self.image = self.up_image
+        elif key[pygame.K_DOWN] or key[pygame.K_s]:
+            self.move_y = 2
+
 
     def update(self):
-        pass
+        self.rect.x += self.move_x
+        self.rect.y += self.move_y
 
     def render(self, window):
         window.blit(self.image, self.rect)
 
 
     
-        
+
+
 
 
 class Game:
@@ -35,10 +60,11 @@ class Game:
             if event.type == pygame.QUIT:
                 self.running = False
                 return
+        self.tank.process_input()
 
 
     def update_game_state(self):
-        pass
+        self.tank.update()
 
     def render(self):
         self.main_window.fill(pygame.color.THECOLORS['white'])
