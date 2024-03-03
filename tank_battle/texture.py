6c88dcdda79f93bfa858683b90604466925b175f
artem-reb <artdev274@gmail.com>
2024-03-03 13:06:14 +0300

    Добавил пласт для работы с графикой

---
+++ tank_battle/texture.py
@@ -0,0 +1,17 @@
+import pygame
+from cellpos import CellPos
+from pygame import Rect 
+
+class TileTexture:
+    def __init__(self, texture_file:str, tile_size:int):
+        self.texture = pygame.image.load().convert_alpha()
+        self.tile_Size = tile_size 
+    
+    def get(self, tile_pos=(0,0), angle = None):
+        x,y = CellPos.position_to_pixel(tile_pos)
+        tile_area = Rect(x,y, self.tile_size, self.tile_size)
+        tile = self.texture.subsurface(tile_area)
+        if angle is not None:
+            tile = pygame.transform.rotate(tile, angle)
+        return tile
+
