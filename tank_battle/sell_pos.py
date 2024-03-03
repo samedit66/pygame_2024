4eead89d1365acd16e6d94d18ba1042f281f2e64
VlakBerr <boro46141@gmail.com>
2024-03-03 12:16:47 +0300

    добавил вывод позиции и сравнивание полей

--- tank_battle/sell_pos.py
+++ tank_battle/sell_pos.py
@@ -6,4 +6,11 @@ class CellPos():
     def __init__(self, col, row):
         self.col = col
         self.row = row
-        
+
+    def __str__(self):
+        return f'CellPos(col={self.col}, row={self.row})'
+
+    def __eq__(self, other_pos):
+        return (self.col == other_pos.col) and (self.row == other_pos.row)
+
+  
