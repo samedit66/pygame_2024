b333381d8c69eaa6cfa63e1c264eafa58b7aa0ad
wrrrebv <vlasovavasilisa20100@gmail.com>
2024-03-03 12:17:37 +0300

    доблавен вывод позиции

--- tank_battle/CellPos.py
+++ tank_battle/CellPos.py
@@ -3,7 +3,18 @@ from settings import Settings
 
 
 class CellPos():
-    
+
     def __init__(self, col, row):
         self.col = col
         self.row = row
+
+    def __str__(self):
+        return f'CellPos(col={self.row})' 
+
+    def __eq__(self, other_pos):
+        return (self.col == other_pos.col) and (self.row == other_pos.row)
+
+cell_pos1 =  CellPos(10, 10)
+cell_pos2 = CellPos(13, 14)
+print(cell_pos1 == cell_pos2)
+print(cell_pos1)
