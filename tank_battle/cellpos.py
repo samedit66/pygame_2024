dcdc5dbe5d6d17e05d056dd93935de75ad962dc1
artem-reb <artdev274@gmail.com>
2024-03-03 12:20:26 +0300

    Добавил возможность выводить позицию в виде строки и сравнивать позиции между собой

--- tank_battle/cellpos.py
+++ tank_battle/cellpos.py
@@ -1,7 +1,14 @@
-from directopn import Direction
+from direction import Direction
 from settings import Settings 
 
 class CellPos():
     def __init__(self, col, row):
         self.col = col 
         self.row = row
+
+    def __str__(self):
+        return f'CellPos(col={self.col}, row={self.row})'
+
+    def __eq__(self, other_pos):
+        return (self.col == other_pos.col) and (self.row == other.pos.row)
+
