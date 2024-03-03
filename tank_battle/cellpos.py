afbb991dcb1d7dbf12250b9749ae3283d0d930d5
artem-reb <artdev274@gmail.com>
2024-03-03 12:38:09 +0300

    Добавил метод получения соседа у клетки

--- tank_battle/cellpos.py
+++ tank_battle/cellpos.py
@@ -12,3 +12,33 @@ class CellPos():
     def __eq__(self, other_pos):
         return (self.col == other_pos.col) and (self.row == other.pos.row)
 
+    @staticmethod
+    def _is_valid_pos(col, row):
+        return (settings.MIN_ROW_INDEX <= row <= Setiings.MAX_ROW_INDEX) and \
+        (Settings.MIN_COL_INDEX <= row <= Setiings.MAX_COL_INDEX)
+
+    def get_neighbor(self, direction):
+        neighbor_col = self.col 
+        neighbor_row = self.row 
+
+        if direction = Direction.UP:
+            neighbor_row -= 1
+        elif direction = Direction.DOWN:
+            neighbor_row += 1
+        elif direction = Direction.LEFT:
+            neighbor_col -= 1
+        elif direction = Direction.RIGHT:
+            neighbor_col += 1
+
+        if CellPos._is_valid_pos(neighbor_col, neighbor_row):
+            return None
+            
+        return CellPos(neighbor_col, neighbor_row)    
+
+
+
+                        
+
+
+
+
