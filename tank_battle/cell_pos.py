dcd1caa5441cf2de004f4b1e69bf8746cfe2cfac
GStorm888 <denchikevstr@icloud.com>
2024-03-03 12:38:11 +0300

    добавил возможность нахождения позиции соседней клетки

--- tank_battle/cell_pos.py
+++ tank_battle/cell_pos.py
@@ -12,3 +12,26 @@ class CellPos():
     def __eq__(self, other_pos):
         return (self.col == other_pos.col) and (self.row == other_pos.row)
     
+    @staticmethod
+    def _is_valid_pos(col, row):
+        return (Settings.MIN_ROW_INDEX <= row <= Settings.MAX_ROW_INDEX) and \
+        (Settings.MIN_COL_INDEX <= col <= Settings.MAX_COL_INDEX)
+
+    def get_neighbor(self, direction):
+        neighbor_col = self.col
+        neighbor_row = self.row
+
+        if direction == Direcction.UP:
+            neighbor_row -= 1
+        elif direction == Direcction.DOWN:
+            neighbor_row += 1
+        elif direction == Direcction.LEFT:
+            neighbor_col -= 1
+        elif direction == Direcction.RIGHT:
+            neighbor_col += 1
+            
+        # если в ходе вычислений получилась неправильная позиция, то веернуть признак, что соседа нет
+        if CellPos._is_valid_pos(neighbor_col, neighbor_row):
+            return None
+        
+        return CellPos(neighbor_col, neighbor_row)
