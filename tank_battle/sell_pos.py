cbd9feae2da644753f46ade35979048f8b0b99a6
VlakBerr <boro46141@gmail.com>
2024-03-03 12:38:46 +0300

    добавил возможность нахождения соседней клетки

--- tank_battle/sell_pos.py
+++ tank_battle/sell_pos.py
@@ -13,4 +13,30 @@ class CellPos():
     def __eq__(self, other_pos):
         return (self.col == other_pos.col) and (self.row == other_pos.row)
 
-  
+    @staticmethod
+    def _is_valid_pos(col, row):
+        return (Settings.MIN_ROW_INDEX <= row <= Settings.MAX_ROW_INDEX) and \
+            (Settings.MIN_COL_INDEX <= col <= Settings.MAX_COL_INDEX)
+     
+
+
+    def get_neighbour(self, direction):
+        neighbour_col = self.col
+        neighbour_row = self.row
+
+        if direction == Directions.UP:
+            neighbour_row -= 1
+        elif direction == Directions.DOWN:
+            neighbour_row += 1
+        elif direction == Directions.LEFT:
+            neighbour_col -= 1
+        elif direction == Directions.RIGHT:
+            neighbour_col += 1
+
+        # Если в ходе вычислений получислось неправильная позиция,
+        # вернуть признак, что соседа в заданном направлении нет
+        if CellPos._is_valid_pos(neighbour_col, neighbour_row):
+            return None
+        
+        return CellPos(neighbour_col, neighbour_row)
+    
