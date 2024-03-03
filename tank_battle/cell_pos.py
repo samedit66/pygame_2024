18d2a567bdbd7e4fc1c0120e185a3b32e17557eb
GStorm888 <denchikevstr@icloud.com>
2024-03-03 12:17:15 +0300

    добавил вывод позиции, сравнение позиций

--- tank_battle/cell_pos.py
+++ tank_battle/cell_pos.py
@@ -5,3 +5,10 @@ class CellPos():
     def __init__(self, col, row):
         self.col = col
         self.row = row
+
+    def __str__(self) -> str:
+        return f"CellPos(col={self.col}, row={self.row})"
+
+    def __eq__(self, other_pos):
+        return (self.col == other_pos.col) and (self.row == other_pos.row)
+    
