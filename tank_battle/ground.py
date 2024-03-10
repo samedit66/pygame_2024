b598769244a476d2a732bc66e5818d40978cbb49
artem-reb <artdev274@gmail.com>
2024-03-10 13:21:29 +0300

    Добавили основные элементы ландшафта

---
+++ tank_battle/ground.py
@@ -0,0 +1,34 @@
+from cellpos import CellPos 
+from game_object import GameObject
+
+
+
+
+class Ground(GameObject):
+    def __init__(self, tile_pos, angle = None, position = None):
+        super.__init__('tanks_images/terrain.png', tile_pos, angle, position)
+
+
+class Ground(Ground):
+    def __init__(self, angle = None):
+        super().__init__(tile_pos = CellPos(0,0), angle = angle)
+
+
+class Road(Ground):
+    def __init__(self, angle = None):
+        super().__init__(tile_pos = CellPos(3,0), angle = angle)
+
+
+class Bush(Ground):
+    def __init__(self, angle = None):
+        super().__init__(tile_pos = CellPos(2,0), angle = angle)
+
+
+class RoadCorner(Ground):
+    def __init__(self, angle = None):
+        super().__init__(tile_pos = CellPos(4,0), angle = angle)
+
+
+class TripleRoad(Ground):
+    def __init__(self, angle = None):
+        super().__init__(tile_pos = CellPos(5,0), angle = angle)
