a4fed90c5e7c1b15dda83c785f1ad6963faa5d0d
GStorm888 <denchikevstr@icloud.com>
2024-03-10 13:28:24 +0300

    добавил препятствия

---
+++ tank_battle/obstacle.py
@@ -0,0 +1,12 @@
+from cell_pos import CellPos 
+from game_object import GameObject
+
+
+class Obstacle(GameObject):
+
+    def __init__(self, tile_pos, angle=None, position=None):
+        super().__init__('tanks_images/blue/towers_walls_blank.png', tile_pos, angle=angle, position=position)
+
+class Wall(Obstacle):
+    def __init__(self, tile_pos, angle=None, position=None):
+        super().__init__(tile_pos=CellPos(1, 4), angle=angle, position=position)
