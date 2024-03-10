a5412044a19b1366446650541c78125880f3181a
artem-reb <artdev274@gmail.com>
2024-03-10 13:27:39 +0300

    Добавил припятствия

---
+++ tank_battle/obstacle.py
@@ -0,0 +1,12 @@
+from cellpos import CellPos 
+from game_object import GameObject 
+
+class Obstacle(GameObject):
+
+    def __init__(self, tile_pos, angle = None, position = None):
+        super().__init__('tanks_images/camo/towers_walls.png', tile_pos, angle, position)
+
+    def Wall(Obstacle):
+
+        def __init__(self, position = None, angle = None):
+            super().__init__(tile_pos = CellPos(1,4), angle = angle, position = position)
