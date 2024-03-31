de88ddc95846086e9af590310a16d59ec16e60d2
GStorm888 <denchikevstr@icloud.com>
2024-03-31 13:14:36 +0300

    исправил инкапсуляцию

--- tank_battle/field.py
+++ tank_battle/field.py
@@ -14,9 +14,9 @@ class Field():
 
     def can_move_to(self, position, direction):
         neighbor = position.get_neighbor(direction)
-        return (neighbor is not None) and (not self._is_occupied(neighbor))
+        return (neighbor is not None) and (not self.is_occupied(neighbor))
 
-    def _is_occupied(self, position):
+    def is_occupied(self, position):
         for unit in self.units:
             if unit.position == position:
                 return True
@@ -26,7 +26,7 @@ class Field():
         return False
         
     def put_at(self, new_unit, position):
-        if self._is_occupied(position):
+        if self.is_occupied(position):
             return False
         
         new_unit.set_field(self)
@@ -34,6 +34,15 @@ class Field():
         self.units.append(new_unit)
         return True
     
+    def get_unit(self, position):
+        for unit in self.units:
+            if unit.position == position:
+                return unit
+        for wall in self.walls:
+            if wall.position == position:
+                return wall
+        return None
+
     def _init_field(self):
         self.ground = [
             [RoadCorner(), RoadCorner(angle=-90), Grass(), Grass(), Bush()],
